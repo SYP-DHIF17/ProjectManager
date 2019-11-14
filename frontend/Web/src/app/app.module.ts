c1f5c41cea68c56b21105182744863ac89e0f923
Elias Trummer <trueld17@htl-kaindorf.at>
2019-11-14 09:09:45 +0100

    small bug fixes

--- frontend/Web/src/app/app.module.ts
+++ frontend/Web/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
