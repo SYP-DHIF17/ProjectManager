662090a0974802c88f0efff1b83f6c8a500b06a9
Elias Trummer <trueld17@htl-kaindorf.at>
2019-11-14 10:45:37 +0100

    started with register component

--- frontend/Web/src/app/app-routing.module.ts
+++ frontend/Web/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: RegisterComponent
   }
 ];
 
--- frontend/Web/src/app/components/register/register.component.ts
+++ frontend/Web/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from 'src/app/providers/loader/loader.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  password_1: string = '';
+  password_2: string = '';
 
-  ngOnInit() {
+  constructor(private loader: LoaderService){
+
+  }
+
+  ngOnInit(): void{
+        
+  }
+
+  onRegister(): void{
+    this.makeRegisterRequest();
+  }
+
+  onKeyDown(): void{
+    this.makeRegisterRequest();
+  }
+
+  private makeRegisterRequest(): void{
+    if(this.email === '' || this.password_1 === '' || this.password_2 == '' ||
+      (this.password_1 != this.password_2)){
+      return;
+    }
+
+    this.loader.setVisible(true);
   }
 
 }
